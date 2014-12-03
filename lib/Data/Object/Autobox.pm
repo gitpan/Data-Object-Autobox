7ee175fed20b2bf9da0a8d558293be492ce978b5
Al Newkirk <awncorp@cpan.org>
2014-12-03 11:03:13 +0000

    Import of AWNCORP/Data-Object-Autobox-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: Data-Object-Autobox
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AWNCORP/Data-Object-Autobox-0.01.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

---
+++ lib/Data/Object/Autobox.pm
@@ -0,0 +1,48 @@
+# ABSTRACT: An Autobox Implementation for Perl 5
+package Data::Object::Autobox;
+
+use 5.10.0;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Data::Object::Autobox - An Autobox Implementation for Perl 5
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+    use Data::Object::Autobox;
+
+=head1 DESCRIPTION
+
+Data::Object::Autobox implements autoboxing via L<Data::Object> to provide
+L<boxing|http://en.wikipedia.org/wiki/Object_type_(object-oriented_programming)>
+for native Perl 5 data types.
+
+=head1 AUTHOR
+
+Al Newkirk <anewkirk@ana.io>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Al Newkirk.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
